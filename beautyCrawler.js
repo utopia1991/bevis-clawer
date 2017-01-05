7a5db7203226b735907a5db1fc7835a87885beb3
Bevis Shen <1304354608@qq.com>
2017-01-05 17:12:24 +0800

    add beauty crawler

--- beautyCrawler.js
+++ beautyCrawler.js
@@ -2,7 +2,7 @@ var http = require('http');                         // http 网路
 var cheerio = require('cheerio');                   // html 解析
 var fs = require("fs");                             // 流
 var queryHref = "http://www.haha.mx/topic/1/new/";  // 设置被查询的目标网址
-var querySearch = 1; // 设置分页位置
+var querySearch = 1;                                // 设置分页位置
 var urls = [];
 
 /*
@@ -14,32 +14,32 @@ function getHtml(href, serach) {
 	var pageData = "";
 	var req = http.get(href + serach, function(res) {
 		res.setEncoding('utf8');
+
 		res.on('data', function(chunk) {
 			pageData += chunk;
 		});
 
 		res.on('end', function() {
 			$ = cheerio.load(pageData);
-			var html = $(".joke-list-item .joke-main-content a img");
-
+			var html = $(".joke-list-item .joke-main-content a img");  // 图片所在页面上的 dom 位置，可根据需求更改
 			for(var i = 0; i < html.length; i++) {
 				var src = html[i].attribs.src;
-				// 筛选部分广告，不是真的段子
-				if (src.indexOf("http://image.haha.mx") > -1) {
+				if (src.indexOf("http://image.haha.mx") > -1) {   // 筛选部分广告，不是真的段子
 					urls.push(html[i].attribs.src)
 				}
 			}
 			if (serach == pagemax) {
-				console.log("图片链接获取完毕！" + urls.length);
-				console.log("链接总数量：" + urls.length);
+				console.log("--- 图片链接获取完毕 ---");
+				console.log("--- 链接总数量：" + urls.length + " ---");
+				console.log("--- 开始下载 ---");
 				if (urls.length > 0) {
 					downImg(urls.shift());
 				} else {
 					console.log("下载完毕");
 				}
 			}
-		});
-	});
+		})
+	})
 }
 
 /**
@@ -51,8 +51,8 @@ function downImg(imgurl) {
 
 	http.get(imgurl.replace("/small/", "/big/"), function(res) {
 		var imgData = "";
-		//一定要设置response的编码为binary否则会下载下来的图片打不开
-		res.setEncoding("binary");
+
+		res.setEncoding("binary");  //一定要设置response的编码为binary否则会下载下来的图片打不开
 
 		res.on("data", function(chunk) {
 			imgData += chunk;
@@ -60,23 +60,24 @@ function downImg(imgurl) {
 
 		res.on("end", function() {
 			var savePath = "./upload/" + narr[0]  + narr[1] + narr[2] + "_" + narr[4];
+
 			fs.writeFile(savePath, imgData, "binary", function(err) {
 				if(err) {
 					console.log(err);
-				}  else {
+				} else {
 					console.log(narr[0]  + narr[1] + narr[2] + "_" + narr[4]);
 					if (urls.length > 0) {
 						downImg(urls.shift());
 					} else {
-						console.log("下载完毕");
+						console.log("--- 下载完毕 ---");
 					}
 				}
-			});
-		});
-	});
+			})
+		})
+	})
 }
 
-var pagemax = 2;    // 获取要下载的页数的内容
+var pagemax = 3;    // 获取要下载的页数的内容
 function start(){
 	console.log("--- 开始获取图片连接 ---");
 	for (var i = 1 ; i <= pagemax ; i++) {
